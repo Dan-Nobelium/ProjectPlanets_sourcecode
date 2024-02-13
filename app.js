5208291008cb02ee4dd7ebc9ffa041aedada304c
Dan-Nobelium <Daniel.nobelium@gmail.com>
2024-02-13 11:59:36 +1100

    some text moved to instructions.js

--- app.js
+++ app.js
@@ -139,25 +139,25 @@ async function saveJsPsychData(outputData) {
     
   //----------------------------------------------------------------------------
   /* instructions text */
-  let consent_text = [
-    '<img src= "./img/logo.png"></img>' +
-      '<p>Welcome to the experiment!</p>' +
-      '<p>Before you begin, please read the information sheet carefully.</p>' +
-      '<br>' +
-      '<p><b>PARTICIPANT INFORMATION STATEMENT AND CONSENT</b></p>' +
-        '<embed src="data/consent/PIS_SONA_3385.pdf" width="800px" height="2100px" />' +
-      '<p>By continuing, you are making a decision whether or not to participate. Clicking the button below indicates that, having read the information provided on the participant information sheet, you consent to the above.' +
-      '<br></p>'
-    ];
+  // let consent_text = [
+  //   '<img src= "./img/logo.png"></img>' +
+  //     '<p>Welcome to the experiment!</p>' +
+  //     '<p>Before you begin, please read the information sheet carefully.</p>' +
+  //     '<br>' +
+  //     '<p><b>PARTICIPANT INFORMATION STATEMENT AND CONSENT</b></p>' +
+  //       '<embed src="data/consent/PIS_SONA_3385.pdf" width="800px" height="2100px" />' +
+  //     '<p>By continuing, you are making a decision whether or not to participate. Clicking the button below indicates that, having read the information provided on the participant information sheet, you consent to the above.' +
+  //     '<br></p>'
+  //   ];
   
-    let demo_text = [
-      '<p> Gender: ' +
-      '<input type="radio" name="gender" value="male" required/> Male &nbsp; ' +
-      '<input type="radio" name="gender" value="female" required/> Female &nbsp;' +
-      '<input type="radio" name="gender" value="other" required/> Other<br>' + '<br>' +
-      '<p> Age: <input name="age" type="text" required/> </p>' + '<br>' +
-      '<p> Native language: <input name="language" type="text" required/> </p>' + '<br>'
-    ];
+    // let demo_text = [
+    //   '<p> Gender: ' +
+    //   '<input type="radio" name="gender" value="male" required/> Male &nbsp; ' +
+    //   '<input type="radio" name="gender" value="female" required/> Female &nbsp;' +
+    //   '<input type="radio" name="gender" value="other" required/> Other<br>' + '<br>' +
+    //   '<p> Age: <input name="age" type="text" required/> </p>' + '<br>' +
+    //   '<p> Native language: <input name="language" type="text" required/> </p>' + '<br>'
+    // ];
     
   
     if (sample === "ProA") {
@@ -550,7 +550,7 @@ async function saveJsPsychData(outputData) {
           phase: 'consent'
         },
         }
-        // timeline.push(consent_block);
+        timeline.push(consent_block);
     
       // demographics
       var demographics_block = {
@@ -561,7 +561,7 @@ async function saveJsPsychData(outputData) {
           phase: 'demographics'
         }
       };
-      // timeline.push(demographics_block);
+      timeline.push(demographics_block);
     
       
   // ----- questionnaires ----- 
--- instructions.js
+++ instructions.js
@@ -1,1 +1,19 @@
-const helloWorld = "Hello World";
+var helloWorld = "Hello World";
+let demo_text = [
+    '<p> Gender: ' +
+    '<input type="radio" name="gender" value="male" required/> Male &nbsp; ' +
+    '<input type="radio" name="gender" value="female" required/> Female &nbsp;' +
+    '<input type="radio" name="gender" value="other" required/> Other<br>' + '<br>' +
+    '<p> Age: <input name="age" type="text" required/> </p>' + '<br>' +
+    '<p> Native language: <input name="language" type="text" required/> </p>' + '<br>'
+  ];
+  let consent_text = [
+    '<img src= "./img/logo.png"></img>' +
+      '<p>Welcome to the experiment!</p>' +
+      '<p>Before you begin, please read the information sheet carefully.</p>' +
+      '<br>' +
+      '<p><b>PARTICIPANT INFORMATION STATEMENT AND CONSENT</b></p>' +
+        '<embed src="data/consent/PIS_SONA_3385.pdf" width="800px" height="2100px" />' +
+      '<p>By continuing, you are making a decision whether or not to participate. Clicking the button below indicates that, having read the information provided on the participant information sheet, you consent to the above.' +
+      '<br></p>'
+    ];
