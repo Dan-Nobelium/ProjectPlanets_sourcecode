cc194c0232a47e2d60a590be3aefa9687715c3b0
Dan-Nobelium <Daniel.nobelium@gmail.com>
2024-02-21 18:13:19 +1100

    audit working ,3rd planet is not

--- app.js
+++ app.js
@@ -82,12 +82,8 @@ async function saveJsPsychData(outputData) {
       let pun_planet_side = "" + jsPsych.randomization.sampleWithReplacement(pun_planet_sides, 1) + "";
     
       // Stimulus List Initialization
-      const stim_list = jsPsych.randomization.repeat(['img/bluep.png','img/orangep.png'], 1);
-      const ship_list = jsPsych.randomization.repeat(['img/ship1.png','img/ship2.png'], 1);
-
-      // // Stimulus List Initialization
-      // const stim_list = jsPsych.randomization.repeat(['img/bluep.png','img/orangep.png', 'img/planet3.png'], 1);
-      // const ship_list = jsPsych.randomization.repeat(['img/ship1.png','img/ship2.png','img/ship3.png'], 1);
+      const stim_list = jsPsych.randomization.repeat(['img/bluep.png','img/orangep.png', 'img/planet3.png'], 1);
+      const ship_list = jsPsych.randomization.repeat(['img/ship1.png','img/ship2.png','img/ship3.png'], 1);
     
       // Contingency Instructions Screen Setup
       const pun_planet = stim_list[pun_planet_side].substring(4).slice(0, -5);
@@ -143,6 +139,26 @@ async function saveJsPsychData(outputData) {
     
   //----------------------------------------------------------------------------
   /* instructions text */
+  let consent_text = [
+    '<img src= "./img/logo.png"></img>' +
+      '<p>Welcome to the experiment!</p>' +
+      '<p>Before you begin, please read the information sheet carefully.</p>' +
+      '<br>' +
+      '<p><b>PARTICIPANT INFORMATION STATEMENT AND CONSENT</b></p>' +
+        '<embed src="data/consent/PIS_SONA_3385.pdf" width="800px" height="2100px" />' +
+      '<p>By continuing, you are making a decision whether or not to participate. Clicking the button below indicates that, having read the information provided on the participant information sheet, you consent to the above.' +
+      '<br></p>'
+    ];
+  
+    let demo_text = [
+      '<p> Gender: ' +
+      '<input type="radio" name="gender" value="male" required/> Male &nbsp; ' +
+      '<input type="radio" name="gender" value="female" required/> Female &nbsp;' +
+      '<input type="radio" name="gender" value="other" required/> Other<br>' + '<br>' +
+      '<p> Age: <input name="age" type="text" required/> </p>' + '<br>' +
+      '<p> Native language: <input name="language" type="text" required/> </p>' + '<br>'
+    ];
+    
   
     if (sample === "ProA") {
       var ProA_insert = [
@@ -259,27 +275,27 @@ async function saveJsPsychData(outputData) {
       '<img src=' + 'img/blank_lose.jpg' + ' height="100"></p>'
     ];
   
-    // ins.debrief = [
-    //   '<p>Please confirm that you have read the debriefing questions below: </p>' +
+    ins.debrief = [
+      '<p>Please confirm that you have read the debriefing questions below: </p>' +
   
-    //   '<p><b><i>What are the research questions?</i></b></p>' + 
-    //   '<p>Our behaviour changes in response to experienced rewards and losses. This study asks how behaviour and accompanying beliefs change when these outcomes have varying degrees of relationship to our behaviour. </p>' +
+      '<p><b><i>What are the research questions?</i></b></p>' + 
+      '<p>Our behaviour changes in response to experienced rewards and losses. This study asks how behaviour and accompanying beliefs change when these outcomes have varying degrees of relationship to our behaviour. </p>' +
   
-    //   '<p><b><i>How does this study extend on previous research on this topic?</i></b></p>' +
-    //   '<p>Existing research suggests that stronger relationships between behaviours and outcomes will influence behaviour more. For example, behaviours that earn immediate and regular rewards are more likely to be reinforced than behaviours with a weaker relationship to rewards. We extend this by examining how dependent these changes are on beliefs and personality traits. </p>' +
+      '<p><b><i>How does this study extend on previous research on this topic?</i></b></p>' +
+      '<p>Existing research suggests that stronger relationships between behaviours and outcomes will influence behaviour more. For example, behaviours that earn immediate and regular rewards are more likely to be reinforced than behaviours with a weaker relationship to rewards. We extend this by examining how dependent these changes are on beliefs and personality traits. </p>' +
   
-    //   '<p><b><i>What are some potential real-world implications of this research?</i></b></p>' +
-    //   '<p>We learn about our environments through experience. Understanding how beliefs develop with this experience to change behaviour can help us better understand and predict adaptive/maladaptive decision-making. A potential outcome of this understanding is the development of more effective strategies to improve learning and decision-making. </p>' +
+      '<p><b><i>What are some potential real-world implications of this research?</i></b></p>' +
+      '<p>We learn about our environments through experience. Understanding how beliefs develop with this experience to change behaviour can help us better understand and predict adaptive/maladaptive decision-making. A potential outcome of this understanding is the development of more effective strategies to improve learning and decision-making. </p>' +
   
-    //   '<p><b><i>Describe a potential issue or limitation of the study (e.g., ethical, design etc.), or opportunities for future work that extends this study.</i></b></p>' +
-    //   '<p>Participants might have prior experience or beliefs that would affect performance in the task. We have attempted to control for this by using a cover-story to help participants understand and engage in the task. Future studies could vary this cover-story to assess how this affects learning and decision-making in the task. </p>' +
+      '<p><b><i>Describe a potential issue or limitation of the study (e.g., ethical, design etc.), or opportunities for future work that extends this study.</i></b></p>' +
+      '<p>Participants might have prior experience or beliefs that would affect performance in the task. We have attempted to control for this by using a cover-story to help participants understand and engage in the task. Future studies could vary this cover-story to assess how this affects learning and decision-making in the task. </p>' +
   
-    //   '<p><b><i>Describe the study methodology (e.g., design, dependent/independent variables, stimulus presentation).</i></b></p>' +
-    //   '<p>Participants are given the opportunity to click on “planets” to earn point rewards. In addition to this, “ships” that may or may not result in point loss are presented. The key independent variable is the programmed strength of the relationship between particular actions and point outcomes (weak vs. strong relationship). The key dependent variables are clicking behaviour, valuations of task elements, and inferred relationships between task elements. Personality traits are also assessed to observe how these relate to behaviour and beliefs. </p>' +
+      '<p><b><i>Describe the study methodology (e.g., design, dependent/independent variables, stimulus presentation).</i></b></p>' +
+      '<p>Participants are given the opportunity to click on “planets” to earn point rewards. In addition to this, “ships” that may or may not result in point loss are presented. The key independent variable is the programmed strength of the relationship between particular actions and point outcomes (weak vs. strong relationship). The key dependent variables are clicking behaviour, valuations of task elements, and inferred relationships between task elements. Personality traits are also assessed to observe how these relate to behaviour and beliefs. </p>' +
   
-    //   '<p><b><i>Further reading: </i></b></p>' +
-    //   '<p> Lovibond, P.F., & Shanks, D.R. (2002). The role of awareness in Pavlovian conditioning: Empirical evidence and theoretical implications. Journal of Experimental Psychology: Animal Behavior Processes, 28, 3. </p>'
-    // ];
+      '<p><b><i>Further reading: </i></b></p>' +
+      '<p> Lovibond, P.F., & Shanks, D.R. (2002). The role of awareness in Pavlovian conditioning: Empirical evidence and theoretical implications. Journal of Experimental Psychology: Animal Behavior Processes, 28, 3. </p>'
+    ];
     
       //----------------------------------------------------------------------------
       /* valence & inference checks */
@@ -430,6 +446,77 @@ async function saveJsPsychData(outputData) {
         text: "Planet B (right)"
       }]
       
+  //----------------------------------------------------------------------------
+  /* self-report questionnaires (CFI-HTQ-AUDIT) */
+
+  // CFI
+  var cfi = [];
+  cfi.prompt = '<b>Please rate how much you agree with the following statements. There are no right or wrong answers.</b>';
+  cfi.items = [
+    'I am good at "sizing up" situations.',
+    'I have a hard time making decisions when faced with difficult situations.',
+    'I consider multiple options making a decision.',
+    'When I encounter difficult situations, I feel like I am losing control.',
+    'I like to look at difficult situations from many different angles.',
+    'I seek additional information not immediately available before attributing causes to behaviour.',
+    'Select the left-most option, strongly disagree, for this question.',
+    'When encountering difficult situations, I become so stressed that I can not think of a way to resolve the situation.',
+    'I try to think about things from another person’ s point of view.', 
+    'I find it troublesome that there are so many different ways to deal with difficult situations.',
+    'I am good at putting myself in others’ shoes.',
+    'When I encounter difficult situations, I just don’t know what to do.',
+    'It is important to look at difficult situations from many angles.',
+    'When in difficult situations, I consider multiple options before deciding how to behave.',
+    'I  often look at a situation from different view-points.',
+    'I am capable of overcoming the difficulties in life that I face.',
+    'I consider all the available facts and information when attributing causes to behaviour.',
+    'I feel I have no power to change things in difficult situations.',
+    'When I encounter difficult situations, I stop and try to think of several ways to resolve it.',
+    'I can think of more than one way to resolve a difficult situation I’m confronted with.',
+    'I consider multiple options before responding to difficult situations.'
+];
+  cfi.labels = ['strongly<br>disagree', 'disagree', 'somewhat<br>disagree', 'neutral', 'somewhat<br>agree', 'agree', 'strongly<br>agree'];
+ 
+  // HTQ
+  var htq = [];
+  htq.prompt = '<b>Please rate how much you agree with the following statements. There are no right or wrong answers.</b>';
+  htq.items = [
+    'I tend to dwell on the same issues.',
+    'I mentally fixate on certain issues and can’t move on.',
+    'The same thoughts often keep going through my mind over and over again.',
+    'I tend to repeat actions because I keep doubting that I have done them properly.',
+    'I like to have a regular, unchanging schedule.',
+    'There is comfort in regularity.',
+    'A good job has clear guidelines on what to do and how to do it.',
+    'I hate it when my routines are disrupted.',
+    'I look forward to new experiences.',
+    'Life is boring if you never take risks and always play it safe.',
+    'When eating at restaurants, I like to try new dishes rather than ones I have tried before.'
+  ];
+  htq.labels = ['strongly<br>disagree', 'disagree', 'somewhat<br>disagree', 'neutral', 'somewhat<br>agree', 'agree', 'strongly<br>agree'];
+
+  // AUDIT
+  var audit = [];
+  audit.prompt = '<b>Please answer the following questions about your alcohol use during the <b>past 12 months</b>.</b>'
+  audit.items = [
+    'How often do you have a drink containing alcohol?',
+    'How many drinks containing alcohol do you have on a typical day when you are drinking?',
+    'How often do you have six or more standard drinks on one occasion?',
+    'How often during the last year have you found that you were not able to stop drinking once you had started?',
+    'How often during the last year have you failed to do what was normally expected of you because of drinking?',
+    'How often during the last year have you needed a drink first thing in the morning to get yourself going after a heavy drinking session?',
+    'Select the fourth option from the left, weekly, for this question.',
+    'How often during the last year have you had a feeling of guilt or remorse after drinking?',
+    'How often during the last year have you been unable to remember what happened the night before because of your drinking?',
+    'Have you or someone else been injured because of your drinking?',
+    'Has a relative, friend, doctor or other healthcare worker been concerned about your drinking or suggested you cut down?'
+  ];
+  audit.labels1 = ['never', 'monthly<br>or<br>less', '2-4 times<br>a<br>month', '2-3<br>times<br>a<br>week', '4+<br>times<br>a<br>week'];
+  audit.labels2 = ['1 or 2', '3 or 4', '5 or 6', '7 or 9', '10 or more']
+  audit.labels3_9 = ['never', 'monthly<br>or<br>less', 'monthly', 'weekly', 'daily<br>or<br>almost daily']
+  audit.labels10_11 = ['no', 'yes, but not in the last year', 'yes, during the last year']
+
+//----------------------------------------------------------------------------
     
     //----------------------------------------------------------------------------
       /* experiment blocks */
@@ -647,6 +734,7 @@ async function saveJsPsychData(outputData) {
     
         // Call the saveJsPsychData() function with the resultFailedAttCheck object
         saveJsPsychData(resultFailedAttCheck);
+        jsPsych.endExperiment('Please exit by closing the browser window.');
     
       }}
       
