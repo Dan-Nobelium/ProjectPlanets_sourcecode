abdd0d336efa02f49e87459c3ee865072cb3f975
Dan-Nobelium <Daniel.nobelium@gmail.com>
2024-03-22 13:51:10 +1100

    lgtm

--- jspsych-6.1.0/plugins/jspsych-survey-multi-catch.js
+++ jspsych-6.1.0/plugins/jspsych-survey-multi-catch.js
@@ -150,7 +150,7 @@ jsPsych.plugins['survey-multi-catch'] = (function() {
     }
 
     // add submit button
-    html += '<input type="submit" id="' + plugin_id_name + '-next" class="' + plugin_id_name + ' jspsych-btn"' + (trial.button_label ? ' value="' + trial.button_label + '"' : '') + '></input>';
+    // html += '<input type="submit" id="' + plugin_id_name + '-next" class="' + plugin_id_name + ' jspsych-btn"' + (trial.button_label ? ' value="' + trial.button_label + '"' : '') + '></input>';
     html += '</form>';
 
     let currentInstructionPage = 0;
